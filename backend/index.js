d2ae6e81e949712f830ea12afd628a4bfb0e8803
Maggie Cheung <maggie.cheung@uconn.edu>
2020-09-01 18:54:01 -0400

    adding upload functionality

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,75 @@
 const express = require('express')
 const {spawn} = require('child_process');
+//var cors = require('cors')
+const bodyParser = require('body-parser');
+const multer = require("multer");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
 const app = express()
 const port = 3000
-app.get('/', (req, res) => {
+
+
+
+//app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
  
+  const handleError = (err, res) => {
+    res
+      .status(500)
+      .contentType("text/plain")
+      .end("Oops! Something went wrong!");
+  };
+  
+  const upload = multer({
+    dest: "/potato/"
+  });
+
+  app.post('/', upload.single( /* name attribute of <file> element in your form */),
+  (req, res) => {
+    // Handle the post for this route
+    console.log("recieved post request");
+        const tempPath = req.file.path;
+        const targetPath = path.join(__dirname, "./uploads/image.jpg");
+    
+        if (path.extname(req.file.originalname).toLowerCase() === ".jpg") {
+          fs.rename(tempPath, targetPath, err => {
+            if (err) return handleError(err, res);
+    
+            res
+              .status(200)
+              .contentType("text/plain")
+              .end("File uploaded!");
+          });
+        } else {
+          fs.unlink(tempPath, err => {
+            if (err) return handleError(err, res);
+    
+            res
+              .status(403)
+              .contentType("text/plain")
+              .end("Only .jpg files are allowed!");
+          });
+        }
+   });
+
+//   var corsOptions = {
+//     origin: 'http://example.com',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
+  
+//   app.get('/products/:id', cors(corsOptions), function (req, res, next) {
+//     res.json({msg: 'This is CORS-enabled for only example.com.'})
+//   })
+
+app.get('/', (req, res) => {
+
  var dataToSend;
  // spawn new child process to call the python script
  const python = spawn('python', ['testscript.py']);
@@ -18,7 +84,12 @@ app.get('/', (req, res) => {
  // send data to browser
  res.send(dataToSend)
  });
- 
+
 })
+app.get("/image.jpg", (req, res) => {
+    res.sendFile(path.join(__dirname, "./uploads/image.jpg"));
+  });
+
 app.listen(port, () => console.log(`Example app listening on port 
 ${port}!`))
+
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  selectedFile: File = null;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event) {
+    this.selectedFile = <File>event.target.files[0];
+  }
+
+  onUpload() {
+    const fd = new FormData();
+    fd.append('image', this.selectedFile, this.selectedFile.name);
+    console.log("fd: ", fd);
+    this.http.post('http://localhost:3000/', fd, {
+      reportProgress: true,
+      observe: 'events'
+    })
+      .subscribe(event => {
+        console.log("res:", event);
+      });
+   }
+
 
   ngOnInit(): void {
   }
