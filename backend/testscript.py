cd945a54fc4c9d6926869f8253868d6f12a5528e
Nic Marchini <nicmarchini@gmail.com>
2020-09-09 19:16:51 -0400

    opening image in py, anime test

--- backend/testscript.py
+++ backend/testscript.py
@@ -1,1 +1,15 @@
+from PIL import Image
+
+im = Image.open(r"uploads/image.jpg")  
+
+#im.show()
+
+print(im)
+
+
+
+#ADD MODEL HERE\
+#PASS IM INTO MODEL RETURN YES OR NO
+
+
 print("hello world")
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import anime from 'animejs/lib/anime.es';
-
+import { default as anime } from '../../../node_modules/animejs/lib/anime.es.js';
 
 @Component({
   selector: 'app-home',
@@ -38,18 +37,67 @@ export class HomeComponent implements OnInit {
     this.imgURL = reader.result;
 
     }
+
+    anime({
+      targets: '#ok .el',
+      translateY: 250,
+      duration: 3000,
+      delay: 1000
+  });
+
+
+   
+
     }
   ngOnInit(): void {    
   }
 
   ngAfterViewInit() {
 
-    anime({
-      targets: '#ok .el',
-      translateY: 250,
-      duration: 3000
-  });
+    
+    var basicTimeline = anime.timeline({
+      autoplay: true
+    });
+    
   
+    basicTimeline
+      .add({
+        targets: ".atext",
+        duration: 1,
+        opacity: "0",
+        delay: 1000
+      })
+      .add({
+        targets: "#ab",
+        duration: 1300,
+        height: 10,
+        width: 300,
+        backgroundColor: "red",
+        border: "0",
+        borderRadius: 100
+      })
+      .add({
+        targets: ".aprogress-bar",
+        duration: 2000,
+        width: 300,
+        easing: "linear"
+      })
+      .add({
+        targets: "#ab",
+        width: 0,
+        duration: 1
+      })
+      .add({
+        targets: ".aprogress-bar",
+        width: 80,
+        height: 80,
+        delay: 500,
+        duration: 750,
+        borderRadius: 80,
+        backgroundColor: "#71DFBE"
+      });
+
+      basicTimeline.play();
   }
 
 }
