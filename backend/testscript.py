d0638f5debcfb07d5b383c81b7ac7004386a4d89
Nic Marchini <nicmarchini@gmail.com>
2020-09-10 03:24:57 -0400

    re-adding model using lfs

--- backend/testscript.py
+++ backend/testscript.py
@@ -1,15 +1,37 @@
-from PIL import Image
+import os
+import sys
+import glob
+import argparse
 
-im = Image.open(r"uploads/image.jpg")  
+import numpy as np
+from tensorflow.keras import backend as K
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.preprocessing import image
 
-#im.show()
+cls_list = ['nototters', 'otters']
 
-print(im)
+# load the trained model
+net = load_model('ottermodel.h5')
 
+image_file = "uploads/image.jpg"
 
+img = image.load_img(image_file, target_size=(224,224))
 
-#ADD MODEL HERE\
-#PASS IM INTO MODEL RETURN YES OR NO
+x = image.img_to_array(img)
+x = preprocess_input(x)
+x = np.expand_dims(x, axis=0)
+pred = net.predict(x)[0]
+top_inds = pred.argsort()[::-1][:5]
+for i in top_inds:
+    #print('    {:.3f}  {}'.format(pred[i], cls_list[i]))
+    pass
 
+if(pred[0]>0.9):
+    res = 'Not an otter!'
+elif(pred[0]<0.1):
+    res = "It's an otter!"
+else:
+    res = "Inconclusive... better predictions coming soon!"
 
-print("hello world")
+print(res)
